77c9f0ec5f5f3e9f3e918588bc6affd812231d72
Bryan Zhao <bryan718@students.cs.ubc.ca>
2023-05-03 04:48:35 -0700

    Allowed new URL to be accepeted.

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ["https://jointwellcaps.onrender.com"];
+const allowedOrigins = [
+  "https://jointwellcaps.onrender.com",
+  "http://www.jointwellcaps.com",
+];
 
 module.exports = allowedOrigins;
