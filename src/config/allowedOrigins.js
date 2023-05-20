dbe0593911a1f3441d1a36e5a2ff90e1e7170faf
Bryan Zhao <bryan718@students.cs.ubc.ca>
2023-05-20 09:30:25 -0700

    Changed the API to allow for one more link.

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "https://jointwellcaps.onrender.com",
   "http://www.jointwellcaps.com",
   "http://localhost:3000",
+  "http://jointwellcaps.com",
 ];
 
 module.exports = allowedOrigins;
